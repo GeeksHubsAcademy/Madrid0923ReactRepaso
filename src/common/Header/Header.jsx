dba05695c5feaa5213f0cac55c6f409f531553a7
Dave86dev <davidochando1986@gmail.com>
2023-11-27 10:54:00 +0100

    Barra de búsqueda según localización en la app

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,10 +1,31 @@
 
 import { CustomNav } from "../CustomNav/CustomNav"
+import { useLocation } from "react-router-dom"
+import { useState, useEffect } from "react"
 import "./Header.css"
 export const Header = () => {
 
+    const location = useLocation()
+
+    const [criteria, setCriteria] = useState("")
+
+    useEffect(()=>{
+        console.log(criteria)
+    },[criteria])
+
     return(
         <div className="headerDesign">
+
+            {
+                location.pathname === "/" &&
+
+                <input 
+                    className="inputSearchDesign"
+                    type="text"
+                    name="criteria"
+                    onChange={()=>setCriteria(e.target.value)}
+                />
+            }
 
             <CustomNav 
                 path="/"
